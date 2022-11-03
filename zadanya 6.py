bf7df93f427270401384ff0b81b7169b11a1536b
Richeansss <117387783+Richeansss@users.noreply.github.com>
2022-11-04 01:13:52 +0700

    Delete zadanya 6.py

--- zadanya 6.py
+++
@@ -1,7 +0,0 @@
-i_mul = 1
-print("Введите число: ")
-str_default_number = input()
-for i_count in range (0, len(str_default_number)):
-    if (str_default_number[i_count] != '0'):
-        i_mul *= int(str_default_number[i_count])
-print(i_mul)
